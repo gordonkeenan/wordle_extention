645ac6aad80b0379a79d15e2b5579f9822bbf50e
gordonkeenan <gordon.keenan@gmail.com>
2025-09-29 13:02:10 +0100

    Add Webpack configuration for building Chrome extension

---
+++ webpack.chrome.config.js
@@ -0,0 +1,32 @@
+// webpack.chrome.config.js
+const path = require('path');
+
+module.exports = {
+    entry: {
+        background: './src/background.js',
+        content: './src/content.js',
+        popup: './src/popup.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.css'],
+    },
+    devtool: 'source-map',
+};
